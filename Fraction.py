e2c21a7aabc14b16521356cc85ee4b5c00234235
mcfc <gregoriodelfinpascua@gmail.com>
2025-01-30 22:05:39 +0800

    Added Source Code Comment Certification
    Added a Comment header on the program as ownership of the code

--- Fraction.py
+++ Fraction.py
@@ -1,3 +1,25 @@
+# Gregorio Delfin P. Pascua
+# 234835
+#
+# Antonth Chrisdale C. Lopez 
+# 233714
+#
+# January 30, 2025
+#
+# We hereby attest to the truth of the following facts:
+#
+# We have not discussed the Python language code in our program
+# with anyone other than our instructor or the teaching assistants
+# assigned to this course.
+#
+# We have not used Python language code obtained from another student,
+# or any other unauthorized source, either modified or unmodified.
+#
+# If any Python language code or documentation used in our program
+# was obtained from another source, such as a textbook or website,
+# that has been clearly noted with a proper citation in the comments
+# of our program.
+
 """
 This is a Fraction Module
 
